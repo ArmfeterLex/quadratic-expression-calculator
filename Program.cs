da8244e7a3aaa903d3f47fc684c7a679c53fff02
ArmfeterLex <alexromanenkobeats@yandex.ru>
2026-03-15 00:39:19 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите два любых числа");
+        double x = double.Parse(Console.ReadLine());
+        double y = double.Parse(Console.ReadLine());
+        double z = Math.Round((x*x)+x*y-(y*y), 2);
+        Console.WriteLine("Квадратичное вычитание значений Х и У = {0:f2}", z);
+        Console.ReadKey();
+    }
+}
